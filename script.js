c63d7d81d151d39ee241f89c9255938cd210d765
Howl404 <114860794+Howl404@users.noreply.github.com>
2023-01-11 21:23:11 +0200

    +

--- script.js
+++ script.js
@@ -1,15 +1,127 @@
-const add = function (a, b) {
-  return a + b;
-};
+let currentOperand = document.getElementsByClassName("current-operand");
+let previousOperand = document.getElementsByClassName("previous-operand");
 
-const subtract = function (a, b) {
-  return a - b;
-};
+let display = "";
+let firstNumber = "";
+let secondNumber = "";
+let switchNumber = false;
+let operator = "";
+let statusOfCalculator = true;
+currentOperand[0].innerHTML = display;
+const numberButtons = document.getElementsByClassName("number");
+const operateButtons = document.getElementsByClassName("operation");
 
-const multiply = function (a, b) {
-  return a * b;
-};
+Array.from(numberButtons).forEach((button) => {
+  button.addEventListener("click", () => {
+    if (display === "" && button.innerHTML == 0) {
+    } else {
+      if (switchNumber === false) {
+        if (
+          (firstNumber.includes(".") && button.innerHTML === ".") ||
+          (button.innerHTML === "." && display === "")
+        ) {
+        } else {
+          display += button.innerHTML;
+          firstNumber += button.innerHTML;
+          currentOperand[0].innerHTML = display;
+        }
+      } else {
+        if (
+          (secondNumber.includes(".") && button.innerHTML === ".") ||
+          (button.innerHTML === "." && display === "")
+        ) {
+        } else {
+          display += button.innerHTML;
+          secondNumber += button.innerHTML;
+          currentOperand[0].innerHTML = display;
+        }
+      }
+    }
+  });
+});
 
-const divide = function (a, b) {
-  return a / b;
-};
+Array.from(operateButtons).forEach((button) => {
+  button.addEventListener("click", () => {
+    switch (button.innerHTML) {
+      case "+":
+        if (statusOfCalculator === true) {
+          display += " + ";
+          currentOperand[0].innerHTML = display;
+          switchNumber = true;
+          operator = "+";
+          statusOfCalculator = false;
+        }
+        break;
+      case "/":
+        if (statusOfCalculator === true) {
+          display += " / ";
+          currentOperand[0].innerHTML = display;
+          switchNumber = true;
+          operator = "/";
+          statusOfCalculator = false;
+        }
+        break;
+      case "-":
+        if (statusOfCalculator === true) {
+          display += " - ";
+          currentOperand[0].innerHTML = display;
+          switchNumber = true;
+          operator = "-";
+          statusOfCalculator = false;
+        }
+        break;
+      case "*":
+        if (statusOfCalculator === true) {
+          display += " * ";
+          currentOperand[0].innerHTML = display;
+          switchNumber = true;
+          operator = "*";
+          statusOfCalculator = false;
+        }
+        break;
+      case "C":
+        display = "";
+        firstNumber = "";
+        secondNumber = "";
+        switchNumber = false;
+        operator = "";
+        currentOperand[0].innerHTML = display;
+        previousOperand[0].innerHTML = "";
+        statusOfCalculator = true;
+        break;
+      case "=":
+        if (firstNumber >= 1 && secondNumber >= 1) {
+          let result = operate(
+            Number(firstNumber),
+            Number(secondNumber),
+            operator
+          );
+          result = +result.toFixed(2);
+          previousOperand[0].innerHTML = display;
+          display = result;
+          currentOperand[0].innerHTML = display;
+          switchNumber = false;
+          firstNumber = result;
+          secondNumber = "";
+          statusOfCalculator = true;
+        } else if (operator === "/" && secondNumber === "0") {
+          previousOperand[0].innerHTML = "";
+          currentOperand[0].innerHTML = "Don't divide by zero.";
+          statusOfCalculator = false;
+        }
+        break;
+    }
+  });
+});
+
+function operate(a, b, op) {
+  if (op === "+") {
+    return a + b;
+  } else if (op === "-") {
+    return a - b;
+  } else if (op === "*") {
+    return a * b;
+  } else if (op === "/") {
+    return a / b;
+  }
+}
